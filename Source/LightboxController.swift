f44d2bb78d8c288c49f90491bcf6a412ee88e27e
Michael Lehenbauer <mikelehen@gmail.com>
2024-07-01 08:44:38 -0700

    Ensure header is visible even with large white images.
    Fixes https://app.asana.com/0/1204781676683007/1206803553364508 (once we pull this into the Shortwave build).

--- Source/LightboxController.swift
+++ Source/LightboxController.swift
@@ -195,7 +195,7 @@ open class LightboxController: UIViewController {
   open override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
 
-    scrollView.frame = view.bounds
+    scrollView.frame = calcScrollViewFrame(view.bounds.size)
     footerView.frame.size = CGSize(
       width: view.bounds.width,
       height: 100
@@ -210,7 +210,7 @@ open class LightboxController: UIViewController {
       x: 0,
       y: 16,
       width: view.bounds.width,
-      height: 100
+      height: 45
     )
     
     if !presented {
@@ -303,10 +303,10 @@ open class LightboxController: UIViewController {
   // MARK: - Layout
 
   open func configureLayout(_ size: CGSize) {
-    scrollView.frame.size = size
+    scrollView.frame = calcScrollViewFrame(size)
     scrollView.contentSize = CGSize(
       width: size.width * CGFloat(numberOfPages) + spacing * CGFloat(numberOfPages - 1),
-      height: size.height)
+      height: scrollView.frame.height)
     scrollView.contentOffset = CGPoint(x: CGFloat(currentPage) * (size.width + spacing), y: 0)
 
     for (index, pageView) in pageViews.enumerated() {
@@ -325,6 +325,16 @@ open class LightboxController: UIViewController {
     overlayView.resizeGradientLayer()
   }
 
+  fileprivate func calcScrollViewFrame(_ size: CGSize) -> CGRect {
+    let headerVisible = headerView.alpha == 1
+    let headerHeight = headerView.frame.minY + headerView.frame.height
+    if (headerVisible) {
+      return CGRect(x: 0, y: headerHeight, width: size.width, height: size.height - headerHeight*2)
+    } else {
+      return CGRect(x: 0, y: 0, width: size.width, height: size.height)
+    }
+  }
+
   fileprivate func loadDynamicBackground(_ image: UIImage) {
     backgroundView.image = image
     backgroundView.layer.add(CATransition(), forKey: "fade")
@@ -339,7 +349,9 @@ open class LightboxController: UIViewController {
       self.headerView.alpha = alpha
       self.footerView.alpha = alpha
       pageView?.playButton.alpha = alpha
-    }, completion: nil)
+    }, completion: {_ in
+      self.configureLayout(self.view.bounds.size)
+    })
   }
 
   // MARK: - Helper functions
